8f24eb21ef701aa048e31ba68084f6810a4f05e9
Jesus Lopez <jlopezr100@gmail.com>
2026-09-22 18:13:20 -0500

    Corregir configuracion de TEMPLATES en settings

--- sistema_asistencia/settings.py
+++ sistema_asistencia/settings.py
@@ -26,6 +26,22 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'sistema_asistencia.urls'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 # Configuración de Base de Datos con Aiven / Local
 DATABASES = {
     'default': dj_database_url.config(
